b7f7d7a868e05b4ea0d9907a87d3a5358d5fdc23
Matthias Kittsteiner <mk@kittmedia.com>
2021-03-18 13:20:33 +0100

    :bookmark: Fix version number

--- embed-privacy.php
+++ embed-privacy.php
@@ -14,7 +14,7 @@ use function strtolower;
 /*
 Plugin Name:	Embed Privacy
 Description:	Embed Privacy prevents from loading external embeds directly and lets the user control which one should be loaded.
-Version:		1.2.3 Alpha 1
+Version:		1.3.0 Alpha 1
 Author:			Epiphyt
 Author URI:		https://epiph.yt
 License:		GPL2
