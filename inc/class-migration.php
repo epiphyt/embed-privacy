b229f9a688242a22bf1e925759553c1a72d5852a
Matthias Kittsteiner <mk@kittmedia.com>
2023-05-18 10:17:27 +0200

    ⚡️ Early check for migration version Closes #172

--- inc/class-migration.php
+++ inc/class-migration.php
@@ -205,19 +205,23 @@ class Migration {
 			return;
 		}
 		
-		// start the migration
-		$this->update_option( 'is_migrating', time() );
-		$this->update_option( 'migration_count', (int) $this->get_option( 'migration_count' ) + 1 );
-		
-		// load textdomain early for migrations
-		load_plugin_textdomain( 'embed-privacy', false, dirname( plugin_basename( Embed_Privacy::get_instance()->plugin_file ) ) . '/languages' );
-		
 		$version = $this->get_option( 'migrate_version', 'initial' );
 		
 		// get legacy network option
 		if ( $version === 'initial' && is_multisite() ) {
 			$version = get_site_option( 'embed_privacy_migrate_version', 'initial' );
 		}
+		
+		if ( $version === $this->version ) {
+			return;
+		}
+		
+		// start the migration
+		$this->update_option( 'is_migrating', time() );
+		$this->update_option( 'migration_count', (int) $this->get_option( 'migration_count' ) + 1 );
+		
+		// load textdomain early for migrations
+		load_plugin_textdomain( 'embed-privacy', false, dirname( plugin_basename( Embed_Privacy::get_instance()->plugin_file ) ) . '/languages' );
 		
 		if ( $version !== $this->version ) {
 			$this->register_default_embed_providers();
