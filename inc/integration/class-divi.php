4ef1033231e407cb80ad41eec71421e8e0628bd8
Matthias Kittsteiner <mk@kittmedia.com>
2024-12-30 11:18:37 +0100

    🐛 Allow script tags while rendering dynamic content in Divi Closes #248

--- inc/integration/class-divi.php
+++ inc/integration/class-divi.php
@@ -16,8 +16,43 @@ final class Divi {
 	 * Initialize functionality.
 	 */
 	public function init() {
-		\add_filter( 'embed_privacy_print_assets', [ self::class, 'enqueue_assets' ] );
 		\add_action( 'embed_privacy_register_assets', [ self::class, 'register_assets' ], 10, 2 );
+		\add_filter( 'embed_privacy_print_assets', [ self::class, 'enqueue_assets' ] );
+		\add_filter( 'et_builder_resolve_dynamic_content', [ self::class, 'add_dynamic_content_filter' ], 5 );
+		\add_filter( 'et_builder_resolve_dynamic_content', [ self::class, 'remove_dynamic_content_filter' ], \PHP_INT_MAX );
+	}
+	
+	/**
+	 * Add filter for dynamic content.
+	 * 
+	 * @since	1.10.9
+	 * 
+	 * @param	string	$content Current dynamic content
+	 * @return	string Current dynamic content
+	 */
+	public static function add_dynamic_content_filter( $content ) {
+		\add_filter( 'wp_kses_allowed_html', [ self::class, 'allow_script_in_post' ], 10, 2 );
+		
+		return $content;
+	}
+	
+	/**
+	 * Allow script tags in post, since Divi runs a wp_kses_post over the embed.
+	 * 
+	 * @since	1.10.9
+	 * 
+	 * @param	array	$html List of allowed HTML tags and attributes
+	 * @param	string	$context Current context
+	 * @return	array Updated list of allowed HTML
+	 */
+	public static function allow_script_in_post( array $html, $context ) {
+		if ( $context === 'post' ) {
+			$html['script'] = [
+				'type' => true,
+			];
+		}
+		
+		return $html;
 	}
 	
 	/**
@@ -46,5 +81,19 @@ final class Divi {
 		$file_version = $is_debug ? \filemtime( \EPI_EMBED_PRIVACY_BASE . 'assets/style/divi' . $suffix . '.css' ) : \EMBED_PRIVACY_VERSION;
 		
 		\wp_register_style( 'embed-privacy-divi', $css_file_url, [], $file_version );
+	}
+	
+	/**
+	 * Remove filter for dynamic content.
+	 * 
+	 * @since	1.10.9
+	 * 
+	 * @param	string	$content Current dynamic content
+	 * @return	string Current dynamic content
+	 */
+	public static function remove_dynamic_content_filter( $content ) {
+		\remove_filter( 'wp_kses_allowed_html', [ self::class, 'allow_script_in_post' ], 10 );
+		
+		return $content;
 	}
 }
