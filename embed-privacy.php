2f46958c5461bd34956e0d921f815f35865b59f2
Matthias Kittsteiner <mk@kittmedia.com>
2025-04-26 10:47:25 +0200

    ♻️ Improve defining plugin constants See #260

--- embed-privacy.php
+++ embed-privacy.php
@@ -32,17 +32,18 @@ along with Embed Privacy. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 
 \define( 'EMBED_PRIVACY_VERSION', '1.10.10' );
 
-if ( ! \defined( 'EPI_EMBED_PRIVACY_BASE' ) ) {
+if ( \file_exists( \WP_PLUGIN_DIR . '/embed-privacy/' ) ) {
 	\define( 'EPI_EMBED_PRIVACY_BASE', \WP_PLUGIN_DIR . '/embed-privacy/' );
 }
-
-if ( ! \defined( 'EPI_EMBED_PRIVACY_FILE' ) ) {
-	\define( 'EPI_EMBED_PRIVACY_FILE', __FILE__ );
+else if ( \file_exists( \WPMU_PLUGIN_DIR . '/embed-privacy/' ) ) {
+	\define( 'EPI_EMBED_PRIVACY_BASE', \WPMU_PLUGIN_DIR . '/embed-privacy/' );
+}
+else {
+	\define( 'EPI_EMBED_PRIVACY_BASE', \plugin_dir_path( __FILE__ ) );
 }
 
-if ( ! \defined( 'EPI_EMBED_PRIVACY_URL' ) ) {
-	\define( 'EPI_EMBED_PRIVACY_URL', \plugin_dir_url( \EPI_EMBED_PRIVACY_BASE . 'embed-privacy.php' ) );
-}
+\define( 'EPI_EMBED_PRIVACY_FILE', __FILE__ );
+\define( 'EPI_EMBED_PRIVACY_URL', \plugin_dir_url( \EPI_EMBED_PRIVACY_BASE . 'embed-privacy.php' ) );
 
 if ( ! \class_exists( 'DOMDocument' ) ) {
 	/**
