43ccff59d01ac8d75394e0c8dc90732212f5d5c5
Matthias Kittsteiner <mk@kittmedia.com>
2025-08-11 09:24:08 +0200

    💄 Add support for all YouTube settings in Elementor Closes #267

--- assets/js/elementor-video.js
+++ assets/js/elementor-video.js
@@ -45,17 +45,30 @@ document.addEventListener( 'DOMContentLoaded', function() {
 				
 				const iframe = document.createElement( 'iframe' );
 				let url = settings.youtube_url.replace( 'watch?v=', 'embed/' ) + '?';
+				const properties = {
+					autoplay: settings.autoplay ? 1 : 0,
+					cc_load_policy: settings.cc_load_policy ? 1 : 0,
+					controls: settings.controls ? 1 : 0,
+					end: settings.end,
+					playsinline: settings.play_on_mobile ? 1 : 0,
+					rel: settings.rel ? 1 : 0,
+					start: settings.start,
+				};
 				
-				if ( settings.controls ) {
-					url += 'controls=1&';
-				}
-				
-				if ( settings.autoplay ) {
-					url += 'autoplay=1&playsinline=1&';
+				for ( const property in properties ) {
+					if ( properties[ property ] === undefined ) {
+						continue;
+					}
+					
+					url += property + '=' + properties[ property ] + '&';
+					
+					if ( property === 'autoplay' && properties[ property ] === 1 && ! properties.playsinline ) {
+						url += 'playsinline=1&';
+					}
 				}
 				
 				// build iframe to replace embed video div
-				iframe.src = url;
+				iframe.src = url.replace( /&$/, '' );
 				iframe.allowFullscreen = 1;
 				iframe.class = 'elementor-video';
 				iframe.style.maxHeight = '332px';
